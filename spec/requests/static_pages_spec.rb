7fa0a7e349807a90c8f1547367d3c75bb6491171
Kevin Gates <kgates@gmail.com>
2012-04-24 14:21:52 -0700

    Modify static controllerr

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+	
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
     it "should have the right title" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
-												:text => "Ruby on Rails Tutorial Sample App | Home")
+												:text => "#{base_title} | Home")
 		end
   end
   
@@ -24,7 +26,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "#{base_title} | Help")
     end
   end
   
@@ -37,7 +39,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "#{base_title} | About Us")
     end
   end
   
@@ -50,7 +52,7 @@ describe "Static pages" do
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+                    :text => "#{base_title} | Contact Us")
     end
   end
 end
